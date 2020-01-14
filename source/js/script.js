5a7fdb64a414f555ca6e18339a7f6c664ef14b0b
ivanovalizz <elizaveta.sm97@gmail.com>
2020-01-14 12:29:44 +0300

    Add display and hidden classes for slider pictures

--- source/js/script.js
+++ source/js/script.js
@@ -13,4 +13,48 @@ window.onload = function() {
       navMain.classList.remove('main-nav_opened');
     }
   });
+};
+
+function slider() {
+  var pictureBefore = document.querySelector('.slider__picture_before');
+  var pictureAfter = document.querySelector('.slider__picture_after');
+  var toggle = document.querySelector('.slider__toggle');
+  var buttonBefore = document.querySelector('.slider__state_before');
+  var buttonAfter = document.querySelector('.slider__state_after');
+
+  buttonAfter.addEventListener('click', function() {
+    if (pictureBefore.classList.contains('slider__before-display')) {
+      pictureBefore.classList.remove('slider__before-display');
+      pictureBefore.classList.add('slider__before-hidden');
+      pictureAfter.classList.remove('slider__after-hidden');
+      pictureAfter.classList.add('slider__after-display');
+    } else {
+      if (pictureBefore.classList.contains('slider__before-hidden')) {
+        pictureAfter.classList.remove('slider__after-hidden');
+        pictureAfter.classList.add('slider__after-display');
+      } else {
+        pictureBefore.classList.remove('slider__before-hidden');
+        pictureAfter.classList.remove('slider__after-hidden');
+        pictureAfter.classList.add('slider__after-display');
+      }
+    }
+  });
+
+  buttonBefore.addEventListener('click', function() {
+    if (pictureAfter.classList.contains('slider__after-display')) {
+      pictureAfter.classList.remove('slider__after-display');
+      pictureAfter.classList.add('slider__after-hidden');
+      pictureBefore.classList.remove('slider__before-hidden');
+      pictureBefore.classList.add('slider__before-display');
+    } else {
+      if (pictureAfter.classList.contains('slider__after-hidden')) {
+        pictureBefore.classList.remove('slider__before-hidden');
+        pictureBefore.classList.add('slider__before-display');
+      } else {
+        pictureAfter.classList.remove('slider__after-hidden');
+        pictureBefore.classList.remove('slider__before-hidden');
+        pictureBefore.classList.add('slider__before-display');
+      }
+    }
+  });
 }
