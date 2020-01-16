09cc08f7362752d8438c14b1c325993b9e024da3
ivanovalizz <elizaveta.sm97@gmail.com>
2020-01-16 12:21:20 +0300

    Fix slider toggle work

--- source/js/script.js
+++ source/js/script.js
@@ -15,49 +15,43 @@ window.onload = function() {
   });
 
   var TABLET_WIDTH = 768;
-  var DESKTOP_WIDTH = 1300;
   var viewportWidth = document.documentElement.clientWidth || window.innerWidth;
-  var example = document.querySelector('.example__content-wrap');
-  var exampleSlider = document.querySelector('.example__slider');
   var pictureBefore = document.querySelector('.slider__picture_before');
   var pictureAfter = document.querySelector('.slider__picture_after');
-  var imageAfter = document.querySelector('picture__image_after');
   var toggle = document.querySelector('.slider__toggle');
   var buttonBefore = document.querySelector('.slider__state_before');
   var buttonAfter = document.querySelector('.slider__state_after');
 
   function mobileSlider() {
     buttonBefore.addEventListener('click', function() {
-      if (pictureBefore.classList.contains('slider__picture-hidden')) {
-        pictureBefore.classList.remove('slider__picture-hidden');
-        pictureAfter.classList.add('slider__picture-hidden');
-        toggle.classList.remove('slider__toggle-after');
+      if (pictureBefore.classList.contains('slider__picture_hidden')) {
+        pictureBefore.classList.remove('slider__picture_hidden');
+        pictureAfter.classList.add('slider__picture_hidden');
+        toggle.style.left = '8%';
       }
     });
 
     buttonAfter.addEventListener('click', function() {
-      if (pictureAfter.classList.contains('slider__picture-hidden')) {
-        pictureAfter.classList.remove('slider__picture-hidden');
-        pictureBefore.classList.add('slider__picture-hidden');
-        toggle.classList.add('slider__toggle-after');
+      if (pictureAfter.classList.contains('slider__picture_hidden')) {
+        pictureAfter.classList.remove('slider__picture_hidden');
+        pictureBefore.classList.add('slider__picture_hidden');
+        toggle.style.left = '48%';
       }
     });
   }
 
   function tabletSlider() {
-    pictureAfter.classList.remove('slider__picture-hidden');
+    pictureAfter.classList.remove('slider__picture_hidden');
 
     buttonBefore.addEventListener('click', function() {
       pictureAfter.style.width = '0%';
       pictureBefore.style.width = '100%';
-      toggle.classList.remove('slider__toggle-after');
-      toggle.classList.add('slider__toggle-before');
+      toggle.style.left = '0%';
     });
     buttonAfter.addEventListener('click', function() {
       pictureBefore.style.width = '0%';
       pictureAfter.style.width = '100%';
-      toggle.classList.remove('slider__toggle-before');
-      toggle.classList.add('slider__toggle-after');
+      toggle.style.left = '100%';
     });
   }
 
